eff6d69001a7dd2a1146bd36a0d0ef8a3a6833ec
Francisco Jordano <arcturus@ardeenelinfierno.com>
2014-03-27 16:21:53 +0100

    App getting data from gazzetta

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
-
+var list;
 window.onload = function() {
   console.log('Hola Milano!');
+
+  //http://www.gazzetta.it/rss/home.xml
+
+  document.getElementById('addButton').
+    addEventListener('click', fetchData);
+
+  list = document.getElementById('newsList');
+  fetchData();
+}
+
+function fetchData() {
+  var myRequest = new XMLHttpRequest({mozSystem: true});
+  myRequest.open('GET', 'http://www.gazzetta.it/rss/home.xml');
+  myRequest.addEventListener('load', function() {
+    if (myRequest.status === 200) {
+      list.innerHTML = '';
+      var items = myRequest.responseXML.querySelectorAll('item');
+      items = Array.prototype.slice.call(items,0);
+      items.forEach(function(item) {
+        var title = item.getElementsByTagName('title')[0].textContent;
+        var description = item.getElementsByTagName('description')[0].textContent;
+        var photo = item.getElementsByTagName('enclosure')[1].getAttribute('url');
+        var listItem = '<li><aside class="pack-end"><img alt="placeholder" src="' + photo + '"></aside><a href="#"><p>' + title + '</p><p>' + description + '</p></a></li>';
+        list.innerHTML = list.innerHTML + listItem;
+      });
+    }
+  });
+  myRequest.send();
 }
